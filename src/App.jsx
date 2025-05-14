9f08259a8ef8b476da8fd69144e95cb7b6395cc2
maggiewaldauf <eaa24mwa@students.eaaa.dk>
2025-05-14 15:50:09 +0200

    Project base, including Delfinus Design System

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './style.scss';
+
+function App() {
+
+  return (
+    <>
+    <div>
+      <h1>Naturhistorisk Museum</h1>
+      <p>Yayyyy, it works</p>
+    </div>
+    </>
+  )
+}
+
+export default App
